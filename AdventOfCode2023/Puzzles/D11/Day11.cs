739e9abab53d20b8055716740d14362050192f79
Frank Bakker <bakker.frank@gmail.com>
2023-12-11 21:58:35 +0100

    Day11.1

---
+++ AdventOfCode2023/Puzzles/D11/Day11.cs
@@ -0,0 +1,74 @@
+﻿namespace AdventOfCode2023.Puzzles.Day11;
+
+/// <summary>
+/// https://adventofcode.com/2023/day/11
+/// </summary>
+public class Day11
+{
+    [Result(9556896)]
+    [TestCase(result: 374)]
+    [Focus]
+    public static int GetAnswer1(string[] input)
+    {
+        var (galaxies, galaxyInRow, galaxyInColumn) = ScanInput(input);
+        var total = 0;
+        for (var i = 0; i+1 < galaxies.Count; i++)
+        {
+            for (var j = i+1; j < galaxies.Count; j++)
+            {
+                total += (galaxies[i] - galaxies[j]).Distance;
+                total += Expansion(galaxyInRow, galaxies[i].Y, galaxies[j].Y);
+                total += Expansion(galaxyInColumn, galaxies[i].X, galaxies[j].X);            }
+        }
+        return total;
+    }
+
+    static int Expansion(bool[] occupationMap, int start, int end)
+    {
+        // Swap if needed
+        if (start > end) (end, start) = (start, end);
+
+        var count = 0;
+        for (int i = start +1; i < end; i++)
+        {
+            if (!occupationMap[i]) count++;
+        }
+
+        return count;
+    }
+
+    static (List<Vector> galaxies, bool[] galaxyInRow, bool[] galaxyInColumn) ScanInput(string[] input)
+    {
+        List<Vector> galaxies = new List<Vector>();
+        var galaxyInColumn = new bool[input[0].Length];
+        var galaxyInRow = new bool[input.Length];
+        
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] == '#')
+                {
+                    galaxies.Add(new(i,j));
+                    galaxyInColumn[j] = true;
+                    galaxyInRow[i] = true;
+                }
+            }
+        }
+
+        return (galaxies, galaxyInRow, galaxyInColumn);
+    }
+    
+    //[Result(0)]
+    [TestCase(result: 0)]
+    public static long GetAnswer2(string[] input)
+    {
+        return 0;
+    }
+    
+    public record struct Vector(int Y, int X)
+    {
+        public static Vector operator-(Vector a, Vector b) => new(a.Y - b.Y, a.X - b.X);
+        public int Distance => Abs(Y) + Abs(X);
+    }
+}
