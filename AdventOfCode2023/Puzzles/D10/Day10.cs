ceb6b1dab07c663c4128ba45d1231793f930f1e9
Frank Bakker <bakker.frank@gmail.com>
2023-12-11 20:48:13 +0100

    Day10.2

--- AdventOfCode2023/Puzzles/D10/Day10.cs
+++ AdventOfCode2023/Puzzles/D10/Day10.cs
@@ -27,6 +27,74 @@ public class Day10
         
         return distance;
     }
+    
+    [Result(445)]
+    [TestCase(result: 10)]
+    public static long GetAnswer2(string[] input)
+    {
+        var beastLocation = GetBeastPosition(input);
+        ReplaceBeast(input, beastLocation);
+        var mainLoopMap = FindMainLoop(input, beastLocation);
+
+        return input.Select((s, i) => CountTilesInLine(s, mainLoopMap, i)).Sum();
+    }
+
+    private static void ReplaceBeast(string[] input, Vector beastLocation)
+    {
+        var map = new Map(input);
+        var openings = GetStartDirections(map, beastLocation);
+
+        var replaceChar = _pipeTypes
+            .Single(p => openings.Contains(p.Value.Opening1) && openings.Contains(p.Value.Opening2)).Key;
+        input[beastLocation.Y] = input[beastLocation.Y].Replace('S', replaceChar);
+    }
+
+    static int CountTilesInLine(string line, bool[,] mainLoopMap, int y)
+    {
+        int numInsideCells = 0;
+        bool inside = false;
+        char startHlineChar = (char)0;
+        
+        // scan a horizontal line, on the left we will be outside the main loop. Every time we cross the main loop
+        // we will change between inside and outside the loop. Only inside the loop any non-loop tile will count
+
+        for (var x = 0; x < line.Length; x++)
+        {
+            var c = line[x];
+            if (!mainLoopMap[y, x])
+            {
+                if (inside) numInsideCells++;
+            }
+
+            else if (c == '|') inside = !inside;
+
+            else if (c == 'L' || c == 'F') startHlineChar = c;
+
+            else if ((startHlineChar, c) is ('L', '7') or ('F', 'J'))
+            {
+                // we crossed a horizontal loop segment that looked like 'L--7'  or 'F---J' meaning we changed sides
+                inside = !inside;
+            }
+        }
+
+        return numInsideCells;
+    }
+
+    static bool[,] FindMainLoop(string[] input, Vector beastLocation)
+    {
+        var result = new bool[input.Length, input[0].Length];
+        var map = new Map(input);
+        var (walker, _) = GetStartWalkers(map, beastLocation);
+
+        while (true)
+        {
+            result[walker.Vector.Y, walker.Vector.X] = true;
+
+            walker = walker.Step();
+            if (walker.Vector == beastLocation) return result; 
+            walker = map.GetPipe(walker.Vector).Turn(walker);            
+        }
+    }
 
     static Vector GetBeastPosition(string[] input)
     {
@@ -41,18 +109,24 @@ public class Day10
     
     static (Walker, Walker) GetStartWalkers(Map map, Vector start)
     {
-        var starts = Enum.GetValues<Direction>().Select(direction =>
+        var starts = GetStartDirections(map, start).Select(d => new Walker(start, d)).ToArray();
+        return (starts[0], starts[1]);
+    }
+    
+    private static Direction[] GetStartDirections(Map map, Vector start)
+    {
+        var openings = Enum.GetValues<Direction>().Select(direction =>
         {
             var vector = GetMoveOffset(direction);
             var pipe = map.TryGetPipe(start + vector);
             if (pipe?.CanEnter(Opposite(direction)) ?? false)
             {
-                return (Walker?)new Walker(start, direction);
+                return (Direction?)direction;
             }
 
             return null;
-        }).OfType<Walker>().ToArray();
-        return (starts[0], starts[1]);
+        }).OfType<Direction>().ToArray();
+        return openings;
     }
 
     static Direction Opposite(Direction direction) => (Direction)(((int)direction + 2) % 4);
@@ -68,19 +142,19 @@ public class Day10
         };
 
     public enum Direction { North, East, South, West }
+
+    static Dictionary<char, Pipe> _pipeTypes = new ()
+    {
+        ['|'] = new (North, South),
+        ['-'] = new (East,  West),
+        ['L'] = new (North, East),
+        ['J'] = new (West, North),
+        ['7'] = new (South, West),
+        ['F'] = new (East, South),
+    };
     
     class Map(string[] input)
     {
-        static Dictionary<char, Pipe> _pipeTypes = new ()
-        {
-            ['|'] = new (North, South),
-            ['-'] = new (East,  West),
-            ['L'] = new (North, East),
-            ['J'] = new (West, North),
-            ['7'] = new (South, West),
-            ['F'] = new (East, South),
-        };
-
         public Pipe? TryGetPipe(Vector v) => _pipeTypes.GetValueOrDefault(input[v.Y][v.X]);
         public Pipe GetPipe(Vector v) => _pipeTypes[input[v.Y][v.X]];
 
@@ -94,13 +168,12 @@ public class Day10
     readonly record struct Walker(Vector Vector, Direction Direction)
     {
         public Walker Step() => this with { Vector = Vector + GetMoveOffset(Direction) };
-    };
+    }
     
-
     public record struct Vector(int Y, int X)
     {
         public static Vector operator+(Vector a, Vector b) => new(a.Y + b.Y, a.X + b.X);
-    };
+    }
 
     readonly record struct Pipe(Direction Opening1, Direction Opening2)
     {
